891400f1dc16b85d0fcf8d1f8c799d5367b0485b
Irinaaa28 <comanirina0128@gmail.com>
2024-04-24 21:10:23 +0300

    I added virtual functions and VPTR

--- main.cpp
+++ main.cpp
@@ -8,10 +8,14 @@ protected:
     std::string garmentColor;
 public:
     Garment(const std::string& garmentCode, const std::string& garmentColor): garmentCode(garmentCode), garmentColor(garmentColor) {}
-    virtual void show() const
+    //virtual function
+    virtual void describe() const
     {
         std::cout << "Garment " << garmentCode << ", color " << garmentColor << std::endl;
     }
+    //pure virtual functions
+    virtual void describe_reverse() const = 0;
+    virtual void garment_type() const = 0;
     std::string getCode() const //getter code
     {
         return garmentCode;
@@ -27,17 +31,97 @@ class Blouse : public Garment
 public:
     //Blouse(const std::string& garmentCode = "", const std::string& garmentColor = ""): Garment(garmentCode, garmentColor) {}
     Blouse(const std::string& garmentCode, const std::string& garmentColor): Garment(garmentCode, garmentColor) {}
-    void show() const override
+    void describe() const override
     {
         std::cout << "Blouse: " << garmentCode << ", color: " << garmentColor << std::endl;
     }
+    void describe_reverse() const override
+    {
+        std::cout << "Color: " << garmentColor << ", blouse: " << garmentCode << std::endl;
+    }
+    void garment_type() const override
+    {
+        std::cout << "This is a blouse" << std::endl;
+    }
 };
 
+class Dress : public Garment
+{
+public:
+    //Dress(const std::string& garmentCode = "", const std::string& garmentColor = ""): Garment(garmentCode, garmentColor) {}
+    Dress(const std::string& garmentCode, const std::string& garmentColor): Garment(garmentCode, garmentColor) {}
+    void describe() const override
+    {
+        std::cout << "Dress: " << garmentCode << ", color: " << garmentColor << std::endl;
+    }
+    void describe_reverse() const override
+    {
+        std::cout << "Color: " << garmentColor << ", dress: " << garmentCode << std::endl;
+    }
+    void garment_type() const override
+    {
+        std::cout << "This is a dress" << std::endl;
+    }
+};
+
+class Skirt : public Garment
+{
+public:
+    //Skirt(const std::string& garmentCode = "", const std::string& garmentColor = ""): Garment(garmentCode, garmentColor) {}
+    Skirt(const std::string& garmentCode, const std::string& garmentColor): Garment(garmentCode, garmentColor) {}
+    void describe() const override
+    {
+        std::cout << "Skirt: " << garmentCode << ", color: " << garmentColor << std::endl;
+    }
+    void describe_reverse() const override
+    {
+        std::cout << "Color: " << garmentColor << ", skirt: " << garmentCode << std::endl;
+    }
+    void garment_type() const override
+    {
+        std::cout << "This is a skirt" << std::endl;
+    }
+};
+
+class Pants : public Garment
+{
+public:
+    //Pants(const std::string& garmentCode = "", const std::string& garmentColor = ""): Garment(garmentCode, garmentColor) {}
+    Pants(const std::string& garmentCode, const std::string& garmentColor): Garment(garmentCode, garmentColor) {}
+    void describe() const override 
+    {
+        std::cout << "Pants: " << garmentCode << ", color: " << garmentColor << std::endl;
+    }
+    void describe_reverse() const override
+    {
+        std::cout << "Color: " << garmentColor << ", pants: " << garmentCode << std::endl;
+    }
+    void garment_type() const override
+    {
+        std::cout << "These are pants" << std::endl;
+    }
+};
 
 int main()
 {
-    Blouse g("#BT1CR", "Red");
-    Garment& blouse1 = g;
-    blouse1.show();
+    Blouse blouse1("#BT1CR", "Red"), blouse2("#BT1CB", "Blue"), blouse3("#BT1CG", "Green"), blouse4("#BT1CY", "Yellow");
+    Dress dress1("#DT1CR", "Red"), dress2("#DT1CB", "Blue"), dress3("#DT1CG", "Green"), dress4("#DT1CY", "Yellow");
+    Skirt skirt1("#ST1CR", "Red"), skirt2("#ST1CB", "Blue"), skirt3("#ST1CG", "Green"), skirt4("#ST1CY", "Yellow");
+    Pants pants1("#PT1CR", "Red"), pants2("#PT1CB", "Blue"), pants3("#PT1CG", "Green"), pants4("#PT1CY", "Yellow");
+    Blouse noneB("none", "none");
+    Dress noneD("none", "none");
+    Skirt noneS("none", "none");
+    Pants noneP("none", "none");
+
+    //function call through reference
+    Garment& b1 = blouse1;
+    b1.describe();
+    b1.describe_reverse();
+    Garment& d1 = dress3;
+    d1.garment_type();
+
+    //VPTR through reference
+    Garment& g1 = dress2;
+    g1.describe();
     return 0;
 }
